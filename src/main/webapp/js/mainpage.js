8f5c58dda087265c816d0e62c7563072fc8d36c3
choihayoon <hayoon6503@gmail.com>
2017-01-14 19:49:28 +0900

    update

--- src/main/webapp/js/mainpage.js
+++ src/main/webapp/js/mainpage.js
@@ -1,9 +1,36 @@
+$(function(){
 
-//board 눌렀을 때 이동
-$("#list_board").click(function() {
-	window.location.href="board.html"
-})
-$(".mypage_btn").click(function(){
-	window.location.href="mypage.html"
-})
+	$.ajax({
+		crossOrigin: true,
+		url : "http://sports.news.naver.com/basketball/news/index.nhn?type=popular",
+		success : function(data){
+			var title = data.split("aside_news_list")[1].split("li")[1].split("<span>")[1].split("</span>")[0];
+			var link = data.split("aside_news_list")[1].split("li")[1].split('"')[1].split('"')[0];
+			$(".content_box1 h3").text(title);
+			$(".content_box1 h3").click(function(){
+				window.location.href = "http://sports.news.naver.com"+link;
+			});
+		}
 
+
+
+
+
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
