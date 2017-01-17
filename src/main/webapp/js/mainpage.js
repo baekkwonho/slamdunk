6227193ef5e323d26a854bede4db46c5abdb7cf4
choihayoon <hayoon6503@gmail.com>
2017-01-17 18:02:41 +0900

    hy

--- src/main/webapp/js/mainpage.js
+++ src/main/webapp/js/mainpage.js
@@ -1,17 +1,16 @@
 $(function(){
 
-
-$.ajax({
-	url : serverAddr+"/news/list.json",
-	method : "GET",
-	dataType : "json",
-	success : function(obj){
+	function ajaxNews(){
+		$.ajax({
+		url : serverAddr+"/news/list.json",
+		method : "GET",
+		dataType : "json",
+		success : function(obj){
 		var result = obj.jsonResult;
 		if(result.state !== "success"){
 				alert("조회 실패입니다.");
 				return;
 			}
-		console.log(result);
 		$(".content_box1 h3").text(result.data.newsTitle);
 		$(".content_box1 h3").click(function(){
 			window.location.href = "http://sports.news.naver.com"+result.data.newsSrc;
@@ -21,8 +20,35 @@ $.ajax({
 		$(".content_box2 img").click(function(){
 			window.location.href ="http://sports.news.naver.com"+result.data.videoSrc;
 		});
-
+		}
+		})
 	}
-})
 
+
+	function ajaxNotice(){
+		$.ajax({
+			url: serverAddr+"/board/boardlist.json",
+			method: "GET",
+			dataType: "json",
+			success: function(obj){
+				var result = obj.jsonResult;
+				if(result.state !== "success"){
+					alert("조회 실패입니다.");
+					return;
+				}
+				$(".content_box3 h3").text(result.data.noticeList[0].title);
+				$(".content_box3 h3").attr("data-no",result.data.noticeList[0].no);
+				}
+			})
+	}
+	$(".content_box3 h3").click(function(){
+		window.location.href = "boardform.html?noticeno="+$(this).attr("data-no");
+	});
+
+
+
+
+
+	ajaxNews();
+	ajaxNotice();
 });
